a53cef9ce9d896e40f096406c73c37cb98413353
Alicia MARIN <ma202240@etu.unice.fr>
2016-12-02 03:44:34 +0100

    maj navbar et depart

--- public/components/paysdepart/paysdepart.controller.js
+++ public/components/paysdepart/paysdepart.controller.js
@@ -5,72 +5,80 @@ var app = angular.module('facilitation');
 app.controller('paysdepartCtrl', function ($scope) {
     $scope.data = {};
     $scope.dataNote = [];
-        $scope.countries = ["FR", "BE", "AUS"];
-        $scope.chart = new Highcharts.Chart('container', {
-            chart: {
-                type: 'column'
-            },
+    $scope.selectedCountry = "FR";
+    $scope.risk = "La température a chuté ces derniers jours";
+    $scope.dangerDepart = "Traverser la mer mediterrannee est dangereux";
+    $scope.countries = ["FR", "BE", "AUS"];
+
+    $scope.chart = new Highcharts.Chart('container', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Pourcentage d\'acception en fonction du pays d\'accueil'
+        },
+        subtitle: {
+            text: 'Source: Attract Melee - Nuit de l\'info 2016'
+        },
+        xAxis: {
+            categories: [
+                'EN',
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
             title: {
-                text: 'Pourcentage d\'acception en fonction du pays d\'accueil'
-            },
-            subtitle: {
-                text: 'Source: Attract Melee - Nuit de l\'info 2016'
-            },
-            xAxis: {
-                categories: [
-                    'EN',
-                ],
-                crosshair: true
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Rainfall (mm)'
-                }
-            },
-            tooltip: {
-                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-                footerFormat: '</table>',
-                shared: true,
-                useHTML: true
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0
-                }
-            },
-            series: [{
-                name: 'FR',
-                data: [49.9]
-            }, {
-                name: 'BE',
-                data: [83.6]
+                text: 'Acceptation (%)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'FR',
+            data: [49.9]
+        }, {
+            name: 'BE',
+            data: [83.6]
 
-            }, {
-                name: 'AUS',
-                data: [48.9]
+        }, {
+            name: 'AUS',
+            data: [48.9]
 
-            }]
-        });
-
-        $scope.change = function(countrySearch){
-          $scope.chart.series[0].update({
-            data: [{
-                name: 'FR',
-                data: [9.9]
-            }, {
-                name: 'BE',
-                data: [13.6]
-
-            }, {
-                name: 'AUS',
-                data: [78.9]
-
-            }]
-        }, true);
-          console.log($scope.chart.series[0]);
-        }
+        }]
     });
+
+    $scope.change = function(countrySearch){
+      $scope.chart.update({
+        series: [{
+            country: 'FR',
+            data: [9.9]
+        }, {
+            name: 'BE',
+            data: [13.6]
+
+        }, {
+            name: 'AUS',
+            data: [78.9]
+
+        }]
+    }, true);
+      console.log($scope.chart.series);
+    }
+
+    $scope.setModalDepart = function(countrySearch){
+      alert("coucou");
+    }
+});
